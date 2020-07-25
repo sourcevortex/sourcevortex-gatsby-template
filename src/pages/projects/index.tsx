10132105b0da7b6d8061b2a46306b95d17233f29
Carlos Henrique <chsjr1996@gmail.com>
2020-07-25 13:20:14 -0300

    Add page projects (temporarily disabled in sidebar)

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+// Components
+import { DefaultLayout } from '@Component/DefaultLayout'
+import { SEO } from '@Component/Seo'
+
+// Config
+import { getPage, PagesSummaryEnum as Pg } from '@Root/config/pagesSummary'
+
+const Docs: React.FC = () => (
+  <DefaultLayout page={getPage(Pg.DOCS_PAGE)}>
+    <SEO title="Docs Page" />
+    <h1>Docs: Em desenvolvimento</h1>
+    <Link to="/">Voltar para página principal</Link>
+  </DefaultLayout>
+)
+
+export default Docs
