582ebe9d7ad18e52596c8c7ec7e4e2737aa2f8a2
Carlos Henrique <chsjr1996@gmail.com>
2020-07-26 12:47:26 -0300

    Improve 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,17 +4,19 @@ import { Link } from 'gatsby'
 // Components
 import DefaultLayout from '@Component/DefaultLayout'
 import SEO from '@Component/Seo'
-
-// Configs
-import { getPage, PagesSummaryEnum as Pg } from '@Config/pagesSummary'
+import Image from '@Component/Image'
+import * as S from '@PageStyle/NotFoundStyles'
 
 const NotFoundPage: React.FC = () => {
   return (
-    <DefaultLayout page={getPage(Pg.NOT_FOUND)}>
-      <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      <Link to="/">Go back to the homepage</Link>
+    <DefaultLayout>
+      <SEO title="Página não encontrada" />
+      <h1>Página não encontrada</h1>
+      <S.ImgContainer>
+        <Image src="generic/hat.png" style={{ width: '300px' }} />
+        <S.NotFoundText>Não me lembro desse lugar...</S.NotFoundText>
+      </S.ImgContainer>
+      <Link to="/">Voltar para página principal</Link>
     </DefaultLayout>
   )
 }
---
+++ src/pagesStyles/NotFoundStyles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import Typography from '@material-ui/core/Typography'
+
+export const ImgContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 50px;
+`
+
+export const NotFoundText = styled(Typography)`
+  font-weight: bold !important;
+  font-size: 32px !important;
+`
